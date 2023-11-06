46284f108e8458a7b9f80c65ee466327d502465f
Nassanalytics <nassim.naoui@gmail.com>
2023-11-06 14:46:24 +0100

    Ajout des dernières touches

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,12 @@
-const containerKeyBord = document.getElementById('keybord')
+const containerKeyBordMid = document.getElementById('keyboard-mid')
+const containerKeyBordLeft = document.getElementById('keyboard-left')
+const containerKeyBordRight = document.getElementById('keyboard-right')
 
-const digitCode = [0,1,2,3,4,5,6,7,8,9] 
+const digitCode = [1,2,3,4,5,6,7,8,9] 
 const digitCodeSorted = digitCode.sort((a,b) => b-a);
-const operator = ['.','%','(',')','+','-','/','x','AC','=']
+const operatorLeft = ['%',')','(','AC']
+const operatorRight = ['+','-','/','x']
+const operatorBottom = ['=','.',0]
 
 
 /*for (let index = 0; index < (4*5); index++) {
@@ -15,5 +19,27 @@ digitCodeSorted.forEach(digit => {
     const button = document.createElement('button');
     button.textContent = digit
     button.classList.add('keycap');
-    containerKeyBord.appendChild(button);
+    containerKeyBordMid.appendChild(button);
+});
+
+operatorBottom.forEach(digit => {
+    const button = document.createElement('button');
+    button.textContent = digit
+    button.classList.add('keycap');
+    containerKeyBordMid.appendChild(button);
+});
+
+
+operatorRight.forEach(digit => {
+    const button = document.createElement('button');
+    button.textContent = digit
+    button.classList.add('keycap');
+    containerKeyBordRight.appendChild(button);
+});
+
+operatorLeft.forEach(digit => {
+    const button = document.createElement('button');
+    button.textContent = digit
+    button.classList.add('keycap');
+    containerKeyBordLeft.appendChild(button);
 });
