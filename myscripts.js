d92da7a1289a4e33d61486be2ddf17aacba3dae3
Nassanalytics <nassim.naoui@gmail.com>
2023-11-17 20:31:28 +0100

    Changement structure html et css

--- myscripts.js
+++ myscripts.js
@@ -1,43 +1,3 @@
-const containerKeyBordMid = document.getElementById('keyboard-mid')
-const containerKeyBordLeft = document.getElementById('keyboard-left')
-const containerKeyBordRight = document.getElementById('keyboard-right')
-
-const digitCode = [1,2,3,4,5,6,7,8,9] 
-const digitCodeSorted = digitCode.sort((a,b) => b-a);
-const operatorLeft = ['%',')','(','AC']
-const operatorRight = ['+','-','/','x']
-const operatorBottom = ['=','.',0]
-
-digitCodeSorted.forEach(digit => {
-    const button = document.createElement('button');
-    button.textContent = digit
-    button.classList.add('keycap');
-    containerKeyBordMid.appendChild(button);
-});
-
-operatorBottom.forEach(digit => {
-    const button = document.createElement('button');
-    button.textContent = digit
-    button.classList.add('keycap');
-    containerKeyBordMid.appendChild(button);
-});
-
-
-operatorRight.forEach(digit => {
-    const button = document.createElement('button');
-    button.textContent = digit
-    button.classList.add('keycap');
-    containerKeyBordRight.appendChild(button);
-});
-
-operatorLeft.forEach(digit => {
-    const button = document.createElement('button');
-    button.textContent = digit
-    button.classList.add('keycap');
-    containerKeyBordLeft.appendChild(button);
-});
-
-
 const add = function (a,b) {
     return a + b;
 }
@@ -71,21 +31,20 @@ function operate (firstNumber,secondNumber) {
     }
 }
 
-
-
 document.addEventListener('DOMContentLoaded',function() {
 
         var display = document.getElementById('display');
-        var digitButton = document.querySelectorAll('#keyboard-mid .keycap');
-    
-    // displaying the first number
-    
-        digitButton.forEach(button => {
-            button.addEventListener('click', function() {
-                    display.value += button.textContent
-                    var number = display.value
-                    console.log(operate(number,5))
+        var digitButton = document.querySelectorAll('.digit');
+
+
+            digitButton.forEach(button => {
+                button.addEventListener('click', function() {
+                    display.innerHTML = '';
+                        display.innerHTML += button.textContent
+                        var number = display.textContent
+                        console.log(operate(number,5))
+                })
             })
-        })
+    
     })
 
